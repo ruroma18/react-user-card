6ef7786c63854f5e992b0c23b035198a2a9e621a
Roman Rudenko <ruroma18@gmail.com>
2022-04-26 17:37:49 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import './App.css';
 import React from 'react';
-import UserCard from './components/UserCards/UserCard';
-import responseData from './data';
+import UserCardList from './components/userCardsList';
 import './index.css';
 
+
 function App() {
-
-  const userData = responseData;
-
-  const userCardItem = userData.map(({ firstName, lastName, profilePicture, description }, index) => {
-    const getFullName = `${firstName.trim()} ${lastName.trim()}`.trim() || 'Person';
-
-    const getInitials = (str) => {
-      const wordsArr = str.split(' ');
-      const initialsArr = wordsArr.map((word) => word[0]);
-      const initialsStr = initialsArr.join(' ');
-      return initialsStr;
-    };
-
-    return (
-        <UserCard
-          key={index}
-          fullName={getFullName}
-          src={profilePicture}
-          alt={`${getFullName} photo`}
-          initials={getInitials(getFullName)}
-          description={description}
-        />
-    )
-
-  }
-
-
-  );
 
   return (
     <>
       <main>
-        <div className="container">
-          <ul className="userCardsContainer">
-            {userCardItem}
-          </ul>
-        </div>
+        <UserCardList />
       </main>
     </>
   );
---
+++ src/components/userCardsList/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import UserCard from "../UserCards";
+import responseData from "../../data";
+
+function UserCardList(props) {
+  const userCardItem = responseData.map(
+    ({ id, firstName, lastName, profilePicture, description }) => {
+      const getFullName =
+        `${firstName.trim()} ${lastName.trim()}`.trim() || "Person";
+
+      const getInitials = (str) => {
+        const wordsArr = str.split(" ");
+        const initialsArr = wordsArr.map((word) => word[0]);
+        const initialsStr = initialsArr.join(" ");
+        return initialsStr;
+      };
+
+      return (
+        <UserCard
+          key={`${id}-${lastName}`}
+          fullName={getFullName}
+          src={profilePicture}
+          alt={`${getFullName} photo`}
+          initials={getInitials(getFullName)}
+          description={description}
+        />
+      );
+    }
+  );
+
+  return (
+    <div className="container">
+      <ul className="userCardsContainer">{userCardItem}</ul>
+    </div>
+  );
+}
+
+export default UserCardList;
